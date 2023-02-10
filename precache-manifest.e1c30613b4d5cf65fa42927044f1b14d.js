1daf61efedd3ac60e8a551826d3dcc66157cb363
sarjit789 <guddideviyadav04@gmail.com>
2023-02-10 21:01:02 +0530

    Updates

---
+++ precache-manifest.e1c30613b4d5cf65fa42927044f1b14d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "76120949478502c00d24",
+    "url": "/Portfolio-Capstone/static/css/main.337628a3.chunk.css"
+  },
+  {
+    "revision": "76120949478502c00d24",
+    "url": "/Portfolio-Capstone/static/js/main.00f4604b.chunk.js"
+  },
+  {
+    "revision": "b5d6b5ab98cddf5bbf81",
+    "url": "/Portfolio-Capstone/static/js/runtime~main.afe1313e.js"
+  },
+  {
+    "revision": "24eea4f14e70b8da84b8",
+    "url": "/Portfolio-Capstone/static/js/2.b53fde1a.chunk.js"
+  },
+  {
+    "revision": "22ee40702ee60e32869a6e80d97cf37b",
+    "url": "/Portfolio-Capstone/static/media/flask.22ee4070.svg"
+  },
+  {
+    "revision": "1109c64899990f3bb31842aa87a41d9a",
+    "url": "/Portfolio-Capstone/static/media/flutter.1109c648.svg"
+  },
+  {
+    "revision": "4336ae61013b18ea587d7544b1e2aafe",
+    "url": "/Portfolio-Capstone/static/media/react.4336ae61.svg"
+  },
+  {
+    "revision": "d9237725db7f9cfd32560a8a93505d4b",
+    "url": "/Portfolio-Capstone/static/media/javascript.d9237725.svg"
+  },
+  {
+    "revision": "3128b5015775262a9a6960363a4e950c",
+    "url": "/Portfolio-Capstone/static/media/p2.3128b501.png"
+  },
+  {
+    "revision": "e2a1d589bab99ac6673662946b4166d9",
+    "url": "/Portfolio-Capstone/static/media/p1.e2a1d589.png"
+  },
+  {
+    "revision": "92dd3384094271061f863f4819221028",
+    "url": "/Portfolio-Capstone/static/media/p3.92dd3384.png"
+  },
+  {
+    "revision": "c34827728751dcef4493ef0f4d9d1916",
+    "url": "/Portfolio-Capstone/static/media/p5.c3482772.png"
+  },
+  {
+    "revision": "5ae47b4e4b83eceae8cb641be626d0ae",
+    "url": "/Portfolio-Capstone/static/media/q.5ae47b4e.png"
+  },
+  {
+    "revision": "57dfc3496da6a66b92d54a1fd106b53c",
+    "url": "/Portfolio-Capstone/static/media/firebase.57dfc349.svg"
+  },
+  {
+    "revision": "96986fcae124e6ff6f2b24ad4f999242",
+    "url": "/Portfolio-Capstone/static/media/nodejs.96986fca.svg"
+  },
+  {
+    "revision": "9bbdfeafa85571580681dd301101bc61",
+    "url": "/Portfolio-Capstone/index.html"
+  }
+];
